4f7bd540aa1c26d5a938dd036bad6e00ce1189a7
spixooze <s_simon_s@hotmail.com>
2018-02-19 11:02:36 +0100

    Starry Background HTML CSS

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import '../styles/App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Grupparbete</h1>
+        </header>
+        <div className="stars" />
+        <div className="twinkling" />
+      </div>
+    )
+  }
+}
+
+export default App
