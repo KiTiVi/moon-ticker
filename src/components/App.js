e9904145a944f2bdf67318d08f6659f90bb993f2
spixooze <s_simon_s@hotmail.com>
2018-02-19 11:29:18 +0100

    background in place

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import '../styles/App.css'
 
+import Background from './Background'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Grupparbete</h1>
+        <header>
+          <h1>Welcome to Grupparbete</h1>
         </header>
-        <div className="stars" />
-        <div className="twinkling" />
+        <Background />
       </div>
     )
   }
