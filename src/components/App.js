19a282d52a191cee5080d23134cec91da531c678
spixooze <s_simon_s@hotmail.com>
2018-02-19 13:55:51 +0100

    En liten månkomponent :) . la till prop-types också.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import '../styles/App.css'
 
 import Background from './Background'
+import Moon from './Moon'
 
 class App extends Component {
   render() {
@@ -10,6 +11,14 @@ class App extends Component {
         <header>
           <h1>Welcome to Grupparbete</h1>
         </header>
+        <Moon
+          animated
+          size={350}
+          position={{
+            top: 100,
+            left: 100
+          }}
+        />
         <Background />
       </div>
     )
