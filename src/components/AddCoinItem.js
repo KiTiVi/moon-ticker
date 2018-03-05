0c9235c9184d0a294baba68b0544c7fd8c32d880
spixooze <s_simon_s@hotmail.com>
2018-03-05 14:08:20 +0100

    lite mer på addCoinItem och filter på listan

--- src/components/AddCoinItem.js
+++ src/components/AddCoinItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 class AddCoinItem extends Component {
   state = {
@@ -6,35 +7,78 @@ class AddCoinItem extends Component {
     error: null
   }
 
-  handleSubmit = () => {
-    this.setState({ error: 'Sket sig' })
+  handleSubmit = e => {
+    const { price_usd, name } = this.props.coin
+    e.preventDefault()
+    if (this.state.value === null) {
+      return this.setState({
+        error: 'Please provide a value'
+      })
+    }
+    if (Number(price_usd) >= this.state.value) {
+      return this.setState({
+        error: `Moon target needs to be greater than ${name}'s current price of $${price_usd}`
+      })
+    }
+
+    // Validation complete - lets submit! :)
     alert(this.state.value)
   }
 
   handleChange = e => {
-    this.setState({ value: e.target.value })
+    const input = e.target.value
+    this.setState({ value: input })
+    if (input == '') {
+      this.setState({ error: null })
+    }
   }
 
   render() {
+    const { name, price_usd } = this.props.coin
+
     return (
-      <tr>
-        <td>{this.props.coin.name}</td>
-        <td>${this.props.coin.price_usd}</td>
-        <td>
-          <input
-            type="number"
-            value={this.state.value}
-            onChange={this.handleChange}
-            required
-          />
-          {this.state.error ? <div>{this.state.error}</div> : null}
-        </td>
-        <td>
+      <CoinRow>
+        <CoinIcon>HEJ</CoinIcon>
+        <CoinName>{name}</CoinName>
+        <CoinPrice>${price_usd}</CoinPrice>
+        <CoinInput>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="number"
+              value={this.state.value}
+              onChange={this.handleChange}
+              step="0.00001"
+              required
+            />
+          </form>
           <button onClick={this.handleSubmit}>ADD</button>
-        </td>
-      </tr>
+          {this.state.error ? <div>{this.state.error}</div> : null}
+        </CoinInput>
+
+        <td />
+      </CoinRow>
     )
   }
 }
 
 export default AddCoinItem
+
+const CoinRow = styled.tr`
+  display: flex;
+  width: 100%;
+  height: 50px;
+`
+const CoinIcon = styled.figure`
+  width: 20%;
+  margin: 0;
+`
+const CoinName = styled.td`
+  width: 20%;
+`
+const CoinPrice = styled.td`
+  width: 20%;
+`
+const CoinInput = styled.td`
+  display: flex;
+  width: 40%;
+`
--- src/components/Addcoin.js
+++ src/components/Addcoin.js
@@ -3,12 +3,16 @@ import styled from 'styled-components'
 import '../styles/App.css'
 import AddCoinItem from './AddCoinItem'
 
+const VALID_COINS = ['Stellar', 'Ripple', 'Bitcoin', 'Ethereum', 'TRON']
+
 class AddCoin extends Component {
   renderTableRows = () => {
     console.log(this.props.coinData)
-    return this.props.coinData.map(coin => {
-      return <AddCoinItem coin={coin} key={coin.id} />
-    })
+    return this.props.coinData
+      .filter(coin => VALID_COINS.includes(coin.name))
+      .map(coin => {
+        return <AddCoinItem coin={coin} key={coin.id} />
+      })
   }
 
   render() {
@@ -17,13 +21,12 @@ class AddCoin extends Component {
         <Table>
           <thead>
             <tr>
-              <th>Name</th>
+              {/* {<th>Name</th>
               <th>Value</th>
-              <th>Moon target</th>
-              <th>Add</th>
+              <th>Moon target</th>} */}
             </tr>
           </thead>
-          <tbody>{this.renderTableRows()}</tbody>
+          <TableBody>{this.renderTableRows()}</TableBody>
         </Table>
       </AddCoinContainer>
     )
@@ -43,4 +46,8 @@ const Table = styled.table`
   height: 70vh;
   width: 550px;
   overflow-y: auto;
+`
+const TableBody = styled.tbody`
+  display: flex;
+  flex-wrap: wrap;
 `
